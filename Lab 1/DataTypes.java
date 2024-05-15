bbdd8b8fc62df6bad0aa152a3b9bce525b8690b2
Srinivas_8 <75988506+Ratlavath@users.noreply.github.com>
2024-05-15 15:51:33 +0530

    Add files via upload

---
+++ Lab 1/DataTypes.java
@@ -0,0 +1,26 @@
+package srinivas;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		int a=88;
+		char c='S';
+		byte b=4;
+		short s=44;
+		long l=5894;
+		float f=88;
+		double d=89562147;
+		boolean bo=true;
+		System.out.println("int value:"+a);
+		System.out.println("char value:"+c);
+		System.out.println("byte value:"+b);
+		System.out.println("short value:"+s);
+		System.out.println("long value:"+l);
+		System.out.println("float value:"+f);
+		System.out.println("double value:"+d);
+		System.out.println("boolean value:"+bo);
+		
+
+	}
+
+}
---
+++ Lab 1/Swap.java
@@ -0,0 +1,17 @@
+package srinivas;
+
+public class Swap {
+
+	public static void main(String[] args) {
+		int c;
+		int temp=0;
+		int a=50;
+		int b=100;
+		temp=a;
+		a=b;
+		b=temp;
+		c=a+b;
+		System.out.println("sum:"+c);
+	}
+
+}
